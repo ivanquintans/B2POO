978436dc46b39b0bff78fecd94d36337c5cc58d9
ivanquintans <ivanqg21@gmail.com>
2022-10-25 19:11:51 +0200

    Empezando B2E2

---
+++ B2E2/src/main/java/B2E2/Estancia.java
@@ -0,0 +1,310 @@
+package B2E2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Estancia {
+
+    private String nombre;
+
+    private int planta;
+
+    private Set<Sensor> sensores;
+
+    private float coste;
+
+
+
+    /* Constructores*/
+
+    public Estancia(String nombre, int planta) {
+
+        //debemos también declarar nuestro set mediante un hash set
+        this.sensores = new HashSet<Sensor>();
+        this.coste= 0f;
+
+        if (nombre != null) {
+            this.nombre = nombre;
+
+        }else{
+            this.nombre= "NOT-VALID-NAME";
+
+        }
+        //suponemos que la planta minima es un segundo garaje
+        if(planta>=-2) {
+            this.planta = planta;
+
+        }else{
+            this.planta=Integer.MIN_VALUE; //SI NO ES UNA PLANTA VALIDA LE ASIGNAMOS EL VALOR MINIMO PARA INTEGER
+
+        }
+
+    }
+
+
+    /*Setters*/
+
+    //no tenemos setter en este caso
+
+
+    /*Getters*/
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPlanta() {
+        return planta;
+    }
+
+    public Set<Sensor> getSensores() {
+        return sensores;
+    }
+
+    public float getCoste() {
+        return coste;
+    }
+
+    /*Metodos funcionales*/
+
+
+    /* CRITERIO DE IGUALDAD*/
+    public boolean darAlta(Sensor sensor) {
+
+        //al utilizar un set no deberiamos comprobar si es igual a los elementos existentes ya que set es una lista no ordenada
+        //en la que no puede existir ningun elemento repetido
+
+        //pero si debemos comprobar si las ids son iguales
+
+        for(Sensor i: this.sensores) {
+            if(i.getId().equals(sensor.getId()))  //si los ids son iguales
+                return false;//no lo añadimos y devolvemos false
+
+        }
+
+        //en otro caso lo añadimos y devolvemos false
+        this.sensores.add(sensor); //añadimos el sensor a sensores
+
+        //calculamos el coste como la suma del precio de los sensores
+
+        this.coste += sensor.getPrecio();
+
+
+        return true;
+
+    }
+
+    /* CRITERIO DE IGUALDAD*/
+    /*HASH SET Y COMPROBAR*/
+
+    public boolean darAlta(String id, String tipo, float[] rango, float precio) {
+
+        Sensor nuevo = new Sensor(id, tipo, rango, precio);
+
+        //pero si debemos comprobar si las ids son iguales
+
+        for(Sensor i: this.sensores) {
+            if(i.getId().equals(nuevo.getId()))  //si los ids son iguales
+                return false;//no lo añadimos y devolvemos false
+        }
+        //en otro caso lo añadimos y devolvemos false
+        this.sensores.add(nuevo); //añadimos el sensor a sensores
+        return true;
+    }
+
+    public boolean nuevoDato(String id, float dato) {
+
+        for (Sensor i : sensores) {
+            if (i.getId().equals(id)) {
+                //funcion que añade valores a un sensor (definido en la clase sensor)
+                i.nuevoDato(dato); //añadimos el nuevo dato al sensor deseado
+
+                if(i.getDatos().contains(dato)){
+                    return true;
+                }
+            }
+        }
+        //si no hay ningun sensor con el mismo id que queremos
+        return false;
+    }
+
+    public Set<Sensor> sensoresTipo(String tipo) {
+
+        Set<Sensor> devuleto = new HashSet<Sensor>();
+
+        for (Sensor i : this.sensores) {
+            if (i.getTipo().equals(tipo)) { //si es igual al tipo deseado
+                //asumimos que en nuestro set no hay sensores repetidos
+                devuleto.add(i); //añadimos ese sensor
+            }
+        }
+        return devuleto;
+    }
+
+    /*Tengo que devolver un array de sensores*/
+    /*Doble bucle*/
+
+    public Sensor[] sensorMaxValor(String tipo) {
+
+        ArrayList<Sensor> arraySensores = new ArrayList<>();
+        float max = Float.MIN_VALUE; //asignamos un valor para comprobar el error
+
+        //for para comprobar el maximo
+        for (Sensor i : this.sensores) {
+            if (i.getTipo().equals(tipo)) {
+                if (i.maximo() >= max) {
+                    max = i.maximo(); //el valor maximo pasa a ser el valor del sensor
+                }
+            }
+        }
+        //si el maximo es ese valor quiere decir que los sesnores tiene rango nulo por lo que lo devuelvo vacio
+        if(max == Float.MIN_VALUE) return new Sensor[0];
+
+        //bucle para añadir al array los sensores cuyo valor sea igual al máximo
+        for (Sensor i : this.sensores) {
+            if (i.getTipo().equals(tipo)) {
+                if (i.maximo() == max) {
+                    arraySensores.add(i);
+
+                }
+            }
+
+        }
+        //recorro el arraylist y copio en el vector mi array
+        Sensor[] solucion = new Sensor[arraySensores.size()];
+        for (int i=0;i<arraySensores.size();i++){
+            solucion[i]=arraySensores.get(i);
+        }
+
+        return solucion; //devolvemos el array final
+    }
+
+    //mismo codigo que el de maximos pero para minimos
+    public Sensor[] sensorMinValor(String tipo) {
+
+        ArrayList<Sensor> arraySensores = new ArrayList<>(); //tamaño 10 ya que no tenemos tantos sensores en la estancia
+        float min = Float.MAX_VALUE; //asignamos un valor para comprobar el error
+
+        //for para comprobar el maximo
+        for (Sensor i : this.sensores) {
+            if (i.getTipo().equals(tipo)) {
+                if (i.minimo() <= min) {
+                    min = i.minimo(); //el valor maximo pasa a ser el valor del sensor
+                }
+            }
+        }
+        //si el MINIMO es ese valor quiere decir que los sensores tiene rango nulo por lo que lo devuelvo vacio
+        if(min == Float.MAX_VALUE) return new Sensor[0];
+
+
+        //bucle para añadir al array los sensores cuyo valor sea igual al máximo
+        for (Sensor i : this.sensores) {
+            if (i.getTipo().equals(tipo)) {
+                if (i.minimo() == min) {
+                    arraySensores.add(i);
+                }
+            }
+
+        }
+        //recorro el arraylist y copio en el vector mi array
+        Sensor[] solucion = new Sensor[arraySensores.size()];
+        for (int i=0;i<arraySensores.size();i++){
+            solucion[i]=arraySensores.get(i);
+            }
+
+        return solucion; //devolvemos el array final
+
+    }
+
+    public ArrayList<Float> valoresSuperiores(String id, float valor) {
+
+        ArrayList<Float> valores = new ArrayList<>();//declaramos un array list para poder devolver los valores
+
+        for (Sensor i : sensores) { //recorremos los sensores
+            if (i.getId().equals(id)) { //si el id del sensor es el mismo
+                for (int j = 0; j < i.getDatos().size(); j++) { //recorremos los datos del sensor
+                    if (i.getDatos().get(j) > valor) {
+                        valores.add(i.getDatos().get(j)); //añadimos al array list el valor
+                    }
+                }
+            }
+        }
+
+        return valores;
+    }
+
+    /*Controlar la coma*/
+    public String imprimirSensores() { //la pongo como publica ya que de esta forma en la calse principal puedo imprimir los valores de los sensores. asi hay mas modularidad en el código
+        String dato = "["; //asignamos a nuestro string en primer lugar el corchete
+
+        boolean flag=false;
+        for (Sensor i :this.sensores) {
+            if(flag){
+                dato+= ",";
+            }
+            dato += "\"" + i.getId() + "\"";
+            flag=true;
+        }
+        dato += "]";
+        return dato;
+    }
+
+    public String imprimirSensoresMedia() { //la pongo como publica ya que de esta forma en la calse principal puedo imprimir los valores de los sensores. asi hay mas modularidad en el código
+        String dato = "["; //asignamos a nuestro string en primer lugar el corchete
+        int flag=0;
+
+        for (Sensor i :this.sensores) {
+            if(flag!=0){
+                dato+="\n";
+                dato += "\t\t\t\t\t";
+            }
+            dato += "{" +"\"" + i.getId() + "\"" + "," + "\"" + i.media()+ "\""+"}";
+            flag++;
+        }
+        dato += "]";
+        return dato;
+    }
+    @Override
+    public int hashCode(){
+        int valor=1;
+        final int primo = 67;
+        if(this.nombre.equals("NOT-VALID-NAME"))return 0;
+        return primo*valor + this.nombre.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) { //metodo equals para paquete
+
+        if(this == obj){
+            return true;
+        }
+        if(obj == null){
+            return false;
+        }
+        if(this.getClass() != obj.getClass()){
+            return false;
+        }
+        final Estancia nuevo = (Estancia) obj;
+        if (!this.nombre.equals(nuevo.nombre)){
+            if(!(this.planta == nuevo.planta)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /*Revisar espcios*/
+    /*Acabar esto*/
+    @Override
+    public String toString(){
+        return "{\n" //si se cumple la condicion imprime lo que queremos y si no imprime lo que le mandamos (nada en este caso)
+                + (this.nombre!=null ? " \"nombre\" : \"" +  this.nombre + "\",\n" : "")
+                + (this.planta>=2 ? " \"planta\" : \"" +  this.planta + "\",\n" : "")
+                + (!this.sensores.isEmpty() ? " \"sensores\" : \"" +  imprimirSensores() + "\",\n" : "")
+                + (this.coste>=0 ? " \"coste\" : " +  this.coste + ",\n" : "")
+                + (!this.sensores.isEmpty() ? " \"sensores_media\" : " +  imprimirSensoresMedia() + ",\n" : "")
+
+                + "}";
+    }
+}
+
---
+++ B2E2/src/main/java/B2E2/PrincipalB2E2.java
@@ -0,0 +1,323 @@
+package B2E2;
+
+import java.util.ArrayList;
+
+public class PrincipalB2E2 {
+    public static void main(String[] args) {
+
+        //Apartado A
+        //crear los sensores1,2,3 con los valores del fichero excel
+
+        Sensor sensor1 = new Sensor("100","temperatura",new float[]{-5,34f},54.5f);
+        Sensor sensor2 = new Sensor("101","temperatura",new float[]{-5,40f},48.5f);
+        Sensor sensor3 = new Sensor("200","sonido",new float[]{0,120f},30.5f);
+        Sensor sensor4 = new Sensor("201","sonido",new float[]{0,110f},28.3f);
+
+
+        //APARTADO B
+        //mostramos por pantalla limites superiores y inferiores
+
+        System.out.println("APARTADO B:\n");
+
+        System.out.println("Sensor 1: " + sensor1.limiteInferior() + " , " + sensor1.limiteSuperior());
+        System.out.println("Sensor 2: " + sensor2.limiteInferior() + " , " + sensor2.limiteSuperior());
+        System.out.println("Sensor 3: " + sensor3.limiteInferior() + " , " + sensor3.limiteSuperior());
+        System.out.println("Sensor 4: " + sensor4.limiteInferior() + " , " + sensor4.limiteSuperior());
+
+        //APARTADO C
+        //crear una estancia
+
+        Estancia estancia1 = new Estancia("Aula01",2);
+
+        //APARTADO D
+        //damos de alta a los sensores creadaos
+
+        estancia1.darAlta(sensor1);
+        estancia1.darAlta(sensor2);
+        estancia1.darAlta(sensor3);
+        estancia1.darAlta(sensor4);
+
+        //APARTADO E
+        //introducir los valores de los sensores
+
+        //sensor 1
+
+        sensor1.nuevoDato(23.4f);
+        sensor1.nuevoDato(23.4f);
+        sensor1.nuevoDato(23.5f);
+        sensor1.nuevoDato(24.1f);
+        sensor1.nuevoDato(23.2f);
+        sensor1.nuevoDato(-23.5f);
+        sensor1.nuevoDato(24.6f);
+        sensor1.nuevoDato(-23.8f);
+        sensor1.nuevoDato(23.9f);
+        sensor1.nuevoDato(24.1f);
+        sensor1.nuevoDato(24.3f);
+        sensor1.nuevoDato(25.4f);
+
+        //sensor2
+
+        sensor2.nuevoDato(22.1f);
+        sensor2.nuevoDato(-22.1f);
+        sensor2.nuevoDato(22.5f);
+        sensor2.nuevoDato(22.4f);
+        sensor2.nuevoDato(22.9f);
+        sensor2.nuevoDato(21.2f);
+        sensor2.nuevoDato(21.5f);
+        sensor2.nuevoDato(21.1f);
+        sensor2.nuevoDato(20.9f);
+        sensor2.nuevoDato(-23.1f);
+        sensor2.nuevoDato(22.8f);
+        sensor2.nuevoDato(23.2f);
+
+        //sensor3
+
+        sensor3.nuevoDato(21.4f);
+        sensor3.nuevoDato(-21.3f);
+        sensor3.nuevoDato(21.2f);
+        sensor3.nuevoDato(-21.2f);
+        sensor3.nuevoDato(21.2f);
+        sensor3.nuevoDato(21.5f);
+        sensor3.nuevoDato(22.3f);
+        sensor3.nuevoDato(21.8f);
+        sensor3.nuevoDato(22.1f);
+        sensor3.nuevoDato(21.7f);
+        sensor3.nuevoDato(-21.9f);
+        sensor3.nuevoDato(22.5f);
+
+        //sensor4
+
+        sensor4.nuevoDato(10.1f);
+        sensor4.nuevoDato(10.1f);
+        sensor4.nuevoDato(-5.2f);
+        sensor4.nuevoDato(30.6f);
+        sensor4.nuevoDato(37.8f);
+        sensor4.nuevoDato(45.8f);
+        sensor4.nuevoDato(50.2f);
+        sensor4.nuevoDato(52.1f);
+        sensor4.nuevoDato(46.3f);
+        sensor4.nuevoDato(-10.3f);
+        sensor4.nuevoDato(89.2f);
+        sensor4.nuevoDato(34.3f);
+
+        //APARTADO F
+        //mostramos por pantalla los sensores
+
+        System.out.println("\nAPARTADO F:\n");
+
+        System.out.println("SENSOR1:\n");
+
+        System.out.println(sensor1);
+
+        System.out.println("\nSENSOR2:\n");
+
+        System.out.println(sensor2);
+
+        System.out.println("\nSENSOR3:\n");
+
+        System.out.println(sensor3);
+
+        System.out.println("\nSENSOR4:\n");
+
+        System.out.println(sensor4);
+
+        //APARTADO G
+        //mostrar por pantalla la estancia1
+
+        System.out.println("\nAPARTADO G:\n");
+
+        System.out.println(estancia1);
+
+
+
+        //APARTADO H
+        //dar de alta los sensores 5,6,7
+
+        estancia1.darAlta("202","sonido",new float[]{0,118f},28.4f); //5
+        estancia1.darAlta("301","humedad",new float[]{0,100f},34.3f); //6
+        estancia1.darAlta("302","humedad",new float[]{0,100f},42.5f); //7
+
+        //APARTADO I
+        //mostramos por pantalla limites superiores y inferiores
+
+        System.out.println("\nAPARTADO I\n");
+
+        //tenemos que hacer una funcion que nos permita buscar el sensor que queremos
+
+        ArrayList<String> ids = new ArrayList<>();
+
+        //añadimos los strings que queremos
+        ids.add("202");
+        ids.add("301");
+        ids.add("302");
+
+        System.out.println("Sensor 1: " + sensor1.limiteInferior() + " , " + sensor1.limiteSuperior());
+        System.out.println("Sensor 2: " + sensor2.limiteInferior() + " , " + sensor2.limiteSuperior());
+        System.out.println("Sensor 3: " + sensor3.limiteInferior() + " , " + sensor3.limiteSuperior());
+        System.out.println("Sensor 4: " + sensor4.limiteInferior() + " , " + sensor4.limiteSuperior());
+
+        int j=5;
+        for(Sensor i : estancia1.getSensores()){
+
+            if (ids.contains(i.getId())){
+                System.out.println("Sensor "+j+": " + i.limiteInferior() + " , " + i.limiteSuperior());
+                j++;
+
+            }
+        }
+
+        //APARTADO J
+        //mostramos por pantalla los sensores 5,6,7
+
+        System.out.println("\nAPARTADO J:\n");
+        for(Sensor i : estancia1.getSensores()){
+
+            if (ids.contains(i.getId())){
+                System.out.println("\n"+i);
+
+            }
+        }
+
+        //APARTADO K
+        //mostramos por pantalla la estancia1
+
+        //COMO NO TIENE VALORES ALGUNOS SESNSORES NOS DAN DE MEDIA EL VALOR PARA FLOAT MINIMO
+
+        System.out.println("\nAPARTADO K:\n");
+
+        System.out.println(estancia1);
+
+        //APARTADO l
+        //añadimos los valores
+        System.out.println("\nAPARTADO L:");
+
+        //sensor 5
+
+        System.out.println("\nDATOS SENSOR 5:\n");
+
+        System.out.println( estancia1.nuevoDato("202",5.1f)+ " \n" +
+        estancia1.nuevoDato("202",5.2f) + " \n" +
+        estancia1.nuevoDato("202",3.5f)+ " \n" +
+        estancia1.nuevoDato("202",25.5f)+ " \n" +
+        estancia1.nuevoDato("202",25.5f)+ " \n" +
+        estancia1.nuevoDato("202",-29.3f)+ " \n" +
+        estancia1.nuevoDato("202",35.1f)+ " \n" +
+        estancia1.nuevoDato("202",35.4f)+ " \n" +
+        estancia1.nuevoDato("202",-35.1f)+ " \n" +
+        estancia1.nuevoDato("202",40.1f)+ " \n" +
+        estancia1.nuevoDato("202",49.8f)+ " \n" +
+        estancia1.nuevoDato("202",34.5f));
+
+        //sensor 6
+
+        System.out.println("\nDATOS SENSOR 6:\n");
+
+        System.out.println( estancia1.nuevoDato("301",31.0f) + " \n" +
+        estancia1.nuevoDato("301",31.2f)+ " \n" +
+        estancia1.nuevoDato("301",31.4f)+ " \n" +
+        estancia1.nuevoDato("301",32.1f)+ " \n" +
+        estancia1.nuevoDato("301",32.4f)+ " \n" +
+        estancia1.nuevoDato("301",32.0f)+ " \n" +
+        estancia1.nuevoDato("301",32.4f)+ " \n" +
+        estancia1.nuevoDato("301",33.4f)+ " \n" +
+        estancia1.nuevoDato("301",33.0f)+ " \n" +
+        estancia1.nuevoDato("301",32.8f)+ " \n" +
+        estancia1.nuevoDato("301",32.9f)+ " \n" +
+        estancia1.nuevoDato("301",32.5f));
+
+
+        //sensor 7
+
+        System.out.println("\nDATOS SENSOR 7:\n");
+
+        System.out.println( estancia1.nuevoDato("302",-45.1f) + " \n" +
+        estancia1.nuevoDato("302",-45.2f)+ " \n" +
+        estancia1.nuevoDato("302",-45.2f)+ " \n" +
+        estancia1.nuevoDato("302",-45.2f)+ " \n" +
+        estancia1.nuevoDato("302",34.9f)+ " \n" +
+        estancia1.nuevoDato("302",34.5f)+ " \n" +
+        estancia1.nuevoDato("302",34.6f)+ " \n" +
+        estancia1.nuevoDato("302",38.9f)+ " \n" +
+        estancia1.nuevoDato("302",37.2f)+ " \n" +
+        estancia1.nuevoDato("302",-45.1f)+ " \n" +
+        estancia1.nuevoDato("302",36.4f)+ " \n" +
+        estancia1.nuevoDato("302",35.4f));
+
+        //APARTADO M
+        //mostramos por pantalla la estancia1
+
+        System.out.println("\nAPARTADO M:\n");
+        System.out.println(estancia1);
+
+        //APARTADO N
+        //mostramos por pantalla las caracteristicas de los sensores temperatura
+
+        System.out.println("\nAPARTADO N:\n");
+         for(Sensor i : estancia1.sensoresTipo("temperatura")){
+             System.out.println(i);
+         }
+
+        //APARTADO O
+        //mostramos por pantalla las caracteristicas de los sensores sonido
+
+        System.out.println("\nAPARTADO O:\n");
+        for(Sensor i : estancia1.sensoresTipo("sonido")){
+            System.out.println(i);
+        }
+
+        //APARTADO P
+        //IMPRIMIMOS EL SENSOR CON VALOR MAXIMO Y MINIMO DE TEMPERATURA
+
+        //SENSOR MAX VALOR
+
+        System.out.println("\nAPARTADO P:\n");
+
+        System.out.println("\nSensor Maximo:\n");
+
+        for(Sensor i : estancia1.sensorMaxValor("humedad")){
+            System.out.println(i);
+        }
+
+        System.out.println("\nSensor Minimo:\n");
+
+        for(Sensor i : estancia1.sensorMinValor("humedad")){
+            System.out.println(i);
+        }
+
+        //APARTADO Q
+        //MOSTRAMOS LOS VALORES PARA EL SENSOR CON ID 301 CUYOS VALORES MAYORES QUE 33
+
+        System.out.println("\nAPARTADO Q:\n");
+
+        System.out.println(estancia1.valoresSuperiores("301",33f));
+
+        //APARTADO R
+        //AÑADIR EL SENSOR 8
+
+        System.out.println("\nAPARTADO R:\n");
+
+        System.out.println(estancia1.darAlta("202","sonido",new float[]{0,118f},67.6f));
+
+        //APARTADO S
+        //mostramos por pantalla la estancia1
+
+        System.out.println("\nAPARTADO S:\n");
+        System.out.println(estancia1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
