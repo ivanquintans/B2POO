604b767c86ba84900b31d8889c5169a715cddb88
ivanquintans <ivanqg21@gmail.com>
2022-12-05 19:11:58 +0100

    B3E2 creando instancias

--- B3E2/src/main/java/B3E2/Director.java
+++ B3E2/src/main/java/B3E2/Director.java
@@ -18,7 +18,7 @@ public class Director {
 
     /*Constructor*/
 
-    public Director(String nombre, HashMap<String, Float> cache, Set<String> especialista) {
+    public Director(String nombre, Set<String> especialista,HashMap<String, Float> cache) {
         setNombre(nombre);
         setCache(cache);
         setEspecialista(especialista);
@@ -104,5 +104,123 @@ public class Director {
         return contador;
 
 
+    }
+
+    /**
+     *
+     * @return un Set con los generos con mas cache
+     */
+
+    public Set<String> generoMasCaro(){
+
+        Set<String> generos = new HashSet<>();
+        float maximo=0;
+
+        //iteramos por la lista de generos en los que es especialista
+        for(String genero: this.especialista){
+            if(this.cache.get(genero) > maximo){
+                maximo = this.cache.get(genero);
+                //borramos el set para asegurarnos que esta vacio(cambiamos de maximo)
+                generos.clear();
+                generos.add(genero);
+            //si hay algun valor igual al genero que ya tenemos, lo añadimos
+            }else if(this.cache.get(genero) == maximo) generos.add(genero);
+
+        }
+
+        return generos;
+
+    }
+
+    /**
+     *
+     * @param minimo
+     * @return un hash map con las peliculas que han tenido un beneficio mayor a una determinada cantidad
+     */
+    public HashMap<String,Pelicula> peliculasMasBeneficio(float minimo){
+
+        HashMap<String,Pelicula> peliculasMas = new HashMap<>();
+
+        //no puedo comprobar si el minimo es distint de null
+        //devolvemos vacio el hashmap ya que no hay peliculas
+        if(minimo<=0) return peliculasMas;
+
+        for(Pelicula peli : this.peliculas){
+            //como presupuesto es un float(tengo presupuesto como protected)
+            if(peli.getPresupuesto()>minimo) peliculasMas.put(peli.getNombre(),peli);
+        }
+
+        return peliculasMas;
+    }
+
+    @Override
+    public int hashCode(){
+        int valor=1;
+        final int primo = 67;
+        if(this.nombre.equals("NOT-VALID-NAME"))return 0;
+        return primo*valor + this.nombre.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) { //metodo equals para paquete
+
+        if(this == obj){
+            return true;
+        }
+        if(obj == null){
+            return false;
+        }
+        if(this.getClass() != obj.getClass()){
+            return false;
+        }
+        final Director nuevo = (Director) obj;
+        //Si los nombres de las peliculas no son iguales
+        if (!this.nombre.equals(nuevo.nombre)){
+            return false;
+        }
+        return true;
+    }
+
+    private String imprimirPeliculas() { //funcion que me permite imprimir las peliculas en el to string
+        String dato = "[";
+
+        //controlar la coma
+        boolean flag=false;
+
+        for (Pelicula i :this.peliculas) {
+            if(flag){
+                dato+= ",";
+            }
+            dato += "\"" + i.getNombre() + "\"";
+            flag=true;
+        }
+        dato += "]";
+        return dato;
+    }
+
+    private String imprimirespecialista() { //funcion que me permite imprimir las peliculas en el to string
+        String dato = "[";
+
+        //controlar la coma
+        boolean flag=false;
+
+        for (String  especialista :this.especialista) {
+            if(flag){
+                dato+= ",";
+            }
+            dato += "\"" + especialista + "\"";
+            flag=true;
+        }
+        dato += "]";
+        return dato;
+    }
+    @Override
+    public String toString(){
+        return "{\n" //si se cumple la condicion imprime lo que queremos y si no imprime lo que le mandamos (nada en este caso)
+                + (this.nombre!=null ? " \"nombre\" : \"" +  this.nombre + "\",\n" : "")
+                + (!this.cache.isEmpty() ? " \"cache\" : \"" +  this.cache + "\",\n" : "")
+                + (!this.peliculas.isEmpty() ? " \"peliculas\" : " +  imprimirPeliculas() + ",\n" : "")
+                + (!this.especialista.isEmpty() ? " \"especialista\" : \"" +  imprimirespecialista() + "\",\n" : "")
+                + "}";
     }
 }
--- B3E2/src/main/java/B3E2/PrincipalB3E2.java
+++ B3E2/src/main/java/B3E2/PrincipalB3E2.java
@@ -1,4 +1,212 @@
 package B3E2;
 
+import java.util.*;
+
 public class PrincipalB3E2 {
+    public static void main(String[] args) {
+    //Apartado A
+    //crear las instancias del conjunto de actores del excel
+
+    Actor actor1= new Actor("Cameron Diaz",new ArrayList<String>(Arrays.asList("Steven Spielberg" , "Martin Scorsese")),20);
+    Actor actor2= new Actor("Ben Stiller",new ArrayList<String>(Arrays.asList("Steven Spielberg" , "Martin Scorsese")),15);
+    Actor actor3= new Actor("Matthew McConaughey",new ArrayList<String>(Arrays.asList("Christofer Nolan", "Woody Allen", "Martin Scorsese")),25);
+    Actor actor4= new Actor("Anne Hathaway",new ArrayList<String>(Arrays.asList("Christofer Nolan", "Woody Allen", "Martin Scorsese")),30);
+    Actor actor5= new Actor("Leonardo DiCaprio",new ArrayList<String>(Arrays.asList("Christofer Nolan", "Quentin Tarantino" ,"Clint Eastwood")),50);
+    Actor actor6= new Actor("Marion Cotillard",new ArrayList<String>(Arrays.asList("Christofer Nolan" ,"David Fincher" ,"Pedro Almodóvar" ,"Roger Mitchell")),15);
+    Actor actor7= new Actor("Christian Bale",new ArrayList<String>(Arrays.asList("Quentin Tarantino", "Francis Ford Coppola", "Clint Eastwood", "Tim Burton")),20);
+    Actor actor8= new Actor("Gary Oldman",new ArrayList<String>(Arrays.asList("Martin Scorsese", "James Cameron", "Alejandro Amenábar", "Guillermo del Toro")),20);
+    Actor actor9= new Actor(" Matt Damon",new ArrayList<String>(Arrays.asList("Christofer Nolan", "Greta Gerwig", "Steven Spielberg", "Woody Allen")),40);
+    Actor actor10= new Actor("Daniel Day-Lewis",new ArrayList<String>(Arrays.asList("Quentin Tarantino", "M. Night Shymalan", "Paul Thomas Anderson")),70);
+    Actor actor11= new Actor("John Travolta",new ArrayList<String>(Arrays.asList("Ridley Scott", "Sofia Coppola", "Danny Boyle", "Hermanos Coen")),25);
+    Actor actor12= new Actor("Bruce Willis",new ArrayList<String>(Arrays.asList("Steven Spielberg", "M. Night Shymalan", "Martin Scorsese", "Alfonso Cuarón")),30);
+    Actor actor13= new Actor("Michael Caine",new ArrayList<String>(Arrays.asList("Steven Soderberg", "Sofia Coppola", "Paul Haggis", "Steven Soderberg")),10);
+    Actor actor14= new Actor("Morgan Freeman",new ArrayList<String>(Arrays.asList("Steven Spielberg", "Ridley Scott", "James Cameron", "Martin Scorsese")),15);
+    Actor actor15= new Actor("Jack Nicholson",new ArrayList<String>(Arrays.asList("Ridley Scott", "Jean Jacques Annaud", "Roman Polanski", "Woody Allen")),20);
+    Actor actor16= new Actor("Liam Neeson",new ArrayList<String>(Arrays.asList("Quentin Tarantino", "Sofia Coppola", "J.J. Abrahams", "M. Night Shymalan")),20);
+    Actor actor17= new Actor("Al Pacino",new ArrayList<String>(Arrays.asList("Martin Scorsese", "Christofer Nolan", "J.J. Abrahams", "Sofia Coppola")),50);
+    Actor actor18= new Actor("Julia Roberts",new ArrayList<String>(Arrays.asList("Sofia Coppola", "Quentin Tarantino", "Martin Scorsese", "Clint Eastwood")),80);
+    Actor actor19= new Actor("Hugh Grant",new ArrayList<String>(Arrays.asList("Quentin Tarantino", "Clint Eastwood", "Greta Gerwig", "Mel Gibson")),55);
+    Actor actor20= new Actor("Brad Pitt",new ArrayList<String>(Arrays.asList("Martin Scorsese", "Sofia Coppola", "Paul Haggis")),80);
+
+
+    ComediaRomantica pelicula1 = new ComediaRomantica("Algo pasa con Mary",1998,null,	"Hermanos Farrelly",90);
+    Ficcion pelicula2 = new Ficcion("Interstellar",2014,169,"Christopher Nolan",100);
+    Ficcion pelicula3 = new Ficcion("Origen",2010,148,"Christopher Nolan",300);
+    Ficcion pelicula4 = new Ficcion("El caballero oscuro",2008,152,"Christopher Nolan",500);
+    Thriller pelicula5 = new Thriller("Infiltrados",2006,151,"Martin Scorsese",80);
+    Thriller pelicula6 = new Thriller("Gans of New York",2002,166,"Martin Scorsese",70);
+    ComediaNegra pelicula7 = new ComediaNegra("Pulp Fiction",1994,153,"Quentin Tarantino",50);
+    Psicologico pelicula8 = new Psicologico("El sexto sentido",1999,107,"M. Night Shymalan",90);
+    ComediaRomantica pelicula9 = new ComediaRomantica("Noting Hill",1999,166,"Roger Mitchell",124);
+    Robos pelicula10  = new Robos("Ocean 11",2011,117,"Steven Soderberg",400);
+
+
+        pelicula1.darAlta("principal",new ArrayList<Actor>(Arrays.asList(actor1,actor2)));
+
+        pelicula2.darAlta("principal",new ArrayList<Actor>(Arrays.asList(actor3,actor4)));
+        pelicula2.darAlta("secundario",new ArrayList<Actor>(Arrays.asList(actor9,actor13)));
+
+        pelicula3.darAlta("principal",new ArrayList<Actor>(Arrays.asList(actor5,actor6)));
+        pelicula3.darAlta("secundario",new ArrayList<Actor>(Collections.singletonList(actor13)));
+
+        pelicula4.darAlta("principal",new ArrayList<Actor>(Arrays.asList(actor7,actor8,actor4)));
+        pelicula4.darAlta("secundario",new ArrayList<Actor>(Arrays.asList(actor13,actor14)));
+
+        pelicula5.darAlta("principal",new ArrayList<Actor>(Arrays.asList(actor5,actor9)));
+        pelicula5.darAlta("secundario",new ArrayList<Actor>(Arrays.asList(actor17,actor15)));
+
+        pelicula6.darAlta("principal",new ArrayList<Actor>(Arrays.asList(actor1,actor5,actor10)));
+        pelicula6.darAlta("secundario",new ArrayList<Actor>(Collections.singletonList(actor16)));
+
+        pelicula7.darAlta("principal",new ArrayList<Actor>(Collections.singletonList(actor11)));
+        pelicula7.darAlta("secundario",new ArrayList<Actor>(Collections.singletonList(actor12)));
+
+    /*Actor random xd*/
+        pelicula8.darAlta("principal",new ArrayList<Actor>(Collections.singletonList(actor12)));
+
+        pelicula9.darAlta("principal",new ArrayList<Actor>(Arrays.asList(actor18,actor19)));
+
+        pelicula10.darAlta("principal",new ArrayList<Actor>(Arrays.asList(actor18,actor20,actor9)));
+
+    //APARTADO B
+    //creamos las instancias directores
+
+        Director director1 = new Director("Hermanos Farrelly",new HashSet<String>(){{add("ComediaRomantica");add("ComediaNegra");}},new HashMap<String,Float>(){{put("ComediaRomantica",2f);put("ComediaNegra",1.5f);}});
+        Director director2 = new Director("Christopher Nolan",new HashSet<String>(){{add("Ficcion");add("Guerra");add("Thriller");}},new HashMap<String,Float>(){{put("Ficcion",2f);put("Guerra",2f);put("Thriller",2f);}});
+        Director director3 = new Director("Martin Scorsese",new HashSet<String>(){{add("Thriller");add("Psicologico");}},new HashMap<String,Float>(){{put("ComediaRomantica",2f);}});
+        Director director4= new Director("Quentin Tarantino",new HashSet<String>(){{add("ComediaNegra");add("Ficcion");add("Guerra");}},new HashMap<String,Float>(){{put("ComediaRomantica",2f);}});
+        Director director5 = new Director("M. Night Shymalan",new HashSet<String>(){{add("Psicologico");add("Ficcion");add("TerrorGotico");add("ComediaRomantica");}},new HashMap<String,Float>(){{put("ComediaRomantica",2f);}});
+        Director director6 = new Director("Roger Mitchell",new HashSet<String>(){{add("ComediaRomantica");add("Parodia");}},new HashMap<String,Float>(){{put("ComediaRomantica",2f);}});
+        Director director7 = new Director("Steven Soderberg",new HashSet<String>(){{add("DeRobos");add("Thriller");add("Ficcion");}},new HashMap<String,Float>(){{put("ComediaRomantica",2f);}});
+
+    //APARTADO G
+    //mostrar por pantalla los actroes 1,5,7,8,9
+
+        System.out.println("\nAPARTADO G:\n");
+
+        System.out.println(actor1);
+        System.out.println(actor5);
+        System.out.println(actor7);
+        System.out.println(actor8);
+        System.out.println(actor9);
+        System.out.println(actor10);
+
+    //APARTADO H
+    //Mostrar por pantalla las peliculas en las que haya participado el actor 5
+
+        System.out.println("\nAPARTADO H:\n");
+
+        System.out.println(actor5.getPeliculas());
+
+    //APARTADO I
+    //mostramos por pantalla los actores dirigidos por MArtin Scorsese
+
+        System.out.println("\nAPARTADO I\n");
+
+    /*Mostrar el conjunto de actores no las caracteristicas*/
+        System.out.println(pelicula1.actoresDirigidos("Martin Scorsese"));
+
+
+    //APARTADO J
+    //mostramos por pantalla los actores dirigidos por Christopher Nolan
+
+        System.out.println("\nAPARTADO J:\n");
+
+    /*Mostrar el conjunto de actores no las caracteristicas*/
+        System.out.println(pelicula6.actoresDirigidos("Christopher Nolan"));
+
+    //APARTADO K
+    //mostramos por pantalla si las peliculas 4,5,6,7 han tenido exito
+
+        System.out.println("\nAPARTADO K:\n");
+
+    /*revisar*/
+        System.out.println("Pelicula 4: "+pelicula4.exito());
+        System.out.println("Pelicula 5: "+pelicula5.exito());
+        System.out.println("Pelicula 6: "+pelicula6.exito());
+        System.out.println("Pelicula 7: "+pelicula7.exito());
+
+
+    //APARTADO l
+    /*Enunciado bastante xd*/
+        System.out.println("\nAPARTADO L:");
+
+
+
+    //APARTADO M
+    //mostramos por pantalla el presupuesto de las peliculas 1,2,9,10
+
+        System.out.println("\nAPARTADO M:\n");
+
+    /*en get presupuesto llamar al metodo??*/
+        System.out.println("Pelicula 1: "+pelicula1.presupuesto());
+        System.out.println("Pelicula 2: "+pelicula2.presupuesto());
+        System.out.println("Pelicula 9: "+pelicula9.presupuesto());
+        System.out.println("Pelicula 10: "+pelicula10.presupuesto());
+
+    /*Hacer lo que queda con chris*/
+    //APARTADO N
+    //mostramos por pantalla las caracteristicas de los sensores temperatura
+
+        System.out.println("\nAPARTADO N:\n");
+
+
+    //APARTADO O
+    //mostramos por pantalla las caracteristicas de los sensores sonido
+
+        System.out.println("\nAPARTADO O:\n");
+
+
+    //APARTADO P
+    //IMPRIMIMOS EL SENSOR CON VALOR MAXIMO Y MINIMO DE TEMPERATURA
+
+    //SENSOR MAX VALOR
+
+        System.out.println("\nAPARTADO P:\n");
+
+    //APARTADO Q
+    //MOSTRAMOS LOS VALORES PARA EL SENSOR CON ID 301 CUYOS VALORES MAYORES QUE 33
+
+        System.out.println("\nAPARTADO Q:\n");
+
+
+    //APARTADO R
+    //AÑADIR EL SENSOR 8
+
+        System.out.println("\nAPARTADO R:\n");
+
+
+    //APARTADO S
+    //mostramos por pantalla la estancia1
+
+        System.out.println("\nAPARTADO S:\n");
+
+    //APARTADO T
+    //mostramos por pantalla los valores de los sensores ruido
+
+        System.out.println("\nAPARTADO T:\n");
+
+    //APARTADO U
+    //mostramos por pantalla los identificadores de los sensores con valor mayor a 40
+
+        System.out.println("\nAPARTADO U:\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
